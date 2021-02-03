3b86c5ff9f05620db303beba94b8d5e0946d1ffc
Luke Abell <luke@blueoceanideas.com>
2021-02-02 19:00:49 -0500

    Revert "wip"
    This reverts commit 47e8d8ae8dd99030eb533587c65954f32e768a14.
    
    # Conflicts:
    #	CHANGELOG.md
    #	composer.json

--- src/ProcessSesWebhookJob.php
+++ src/ProcessSesWebhookJob.php
@@ -9,14 +9,11 @@ use Illuminate\Support\Arr;
 use Spatie\Mailcoach\Events\WebhookCallProcessedEvent;
 use Spatie\Mailcoach\Models\Send;
 use Spatie\Mailcoach\Support\Config;
-use Spatie\Mailcoach\Traits\UsesMailcoachModels;
 use Spatie\WebhookClient\Models\WebhookCall;
 use Spatie\WebhookClient\ProcessWebhookJob;
 
 class ProcessSesWebhookJob extends ProcessWebhookJob
 {
-    use UsesMailcoachModels;
-
     public function __construct(WebhookCall $webhookCall)
     {
         parent::__construct($webhookCall);
@@ -49,9 +46,7 @@ class ProcessSesWebhookJob extends ProcessWebhookJob
         }
 
         /** @var \Spatie\Mailcoach\Models\Send $send */
-        $sendModelClass = $this->getSendClass();
-
-        $send = $sendModelClass::findByTransportMessageId($messageId);
+        $send = Send::findByTransportMessageId($messageId);
 
         if (!$send) {
             $this->markAsProcessed();
